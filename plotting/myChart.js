bde9c526eabc606cec57654f2a6db19c562cd59a
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2021-06-14 20:36:35 +0530

    Made JSON file with synthetic data

--- plotting/myChart.js
+++ plotting/myChart.js
@@ -1,22 +1,81 @@
+// https://stackoverflow.com/questions/49059722/chart-js-hover-labels-to-display-data-for-all-data-points-on-x-axis
 
-function renderChart(data, labels) {
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'This week',
-                data: data,
+
+var ctx = document.getElementById("myChart").getContext('2d');
+
+// Define the data 
+var data = [{
+                label: "1",
+                x: 5,
+                y: 4
+            },
+            {
+                label: "1",
+                x: 2,
+                y: 14
+            },
+            {
+                label: "1",
+                x: 4,
+                y: 12
+            },
+            {
+                label: "1",
+                x: 2,
+                y: 10
+            },
+            {
+                label: "1",
+                x: 3,
+                y: 4
+            },
+            {
+                label: "1",
+                x: 3,
+                y: 5
+            },
+            {
+                label: "1",
+                x: 3,
+                y: 8
+            },
+            {
+                label: "1",
+                x: 6,
+                y: 12
+     
+         }]; // Add data values to array
+// End Defining data
+var options = {
+    responsive: true, // Instruct chart js to respond nicely.
+    maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+    hover: {
+         mode: 'index',
+         intersect: false
+    },
+    legend: {
+      display: true,
+      position: 'top',
+      labels: {
+        fontColor: "#000080",
+      }
+    },
+    // tooltips: {
+    //  mode: 'index',
+    //  intersect: false
+    // },
+};
+
+// End Defining data
+var myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+                label: 'Population', // Name the series
+                data: data, // Specify the data values array
+          borderColor: '#2196f3', // Add custom color border            
+          backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
             }]
-        },
-    });
-}
-
-$("#renderBtn").click(
-    function () {
-        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
-        labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-        renderChart(data, labels);
-    }
-);
+    },
+    options: options
+});
---
+++ plotting/myChart2.js
@@ -0,0 +1,34 @@
+// https://stackoverflow.com/a/44661975
+
+var imported_data = require('json!./../../data/plotting_file.json');
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var scatterChart = new Chart(ctx, {
+   type: 'scatter',
+   data: {
+      labels: ["Label 1", "Label 2", "Label 3"],
+      datasets: [{
+         label: 'Legend',
+         data: [{
+            x: -10,
+            y: 0,
+         }, {
+            x: 0,
+            y: 10
+         }, {
+            x: 10,
+            y: 5
+         }]
+      }]
+   },
+   options: {
+      tooltips: {
+         callbacks: {
+            label: function(tooltipItem, data) {
+               var label = data.labels[tooltipItem.index];
+               return label + ': (' + tooltipItem.xLabel + ', ' + tooltipItem.yLabel + ')';
+            }
+         }
+      }
+   }
+});
