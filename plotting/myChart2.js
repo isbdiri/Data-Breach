91e526414553e140565759ea809e1cfee8c10587
deutranium <29124655+deutranium@users.noreply.github.com>
2021-06-15 02:07:05 +0530

    Added import for JSON

--- plotting/myChart2.js
+++ plotting/myChart2.js
@@ -1,37 +1,48 @@
 // https://stackoverflow.com/a/44661975
 
 // var imported_data = require('../data/plotting_file.json');
-define(function (require) {
-   var imported_data = require('../data/plotting_file.json');
-});
+// define(function (require) {
+//    var imported_data = require('../data/plotting_file.json');
+// });
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var scatterChart = new Chart(ctx, {
-   type: 'scatter',
-   data: {
-      labels: ["Label 1", "Label 2", "Label 3"],
-      datasets: [{
-         label: 'Legend',
-         data: [{
-            x: -10,
-            y: 0,
-         }, {
-            x: 0,
-            y: 10
-         }, {
-            x: 10,
-            y: 5
-         }]
-      }]
-   },
-   options: {
-      tooltips: {
-         callbacks: {
-            label: function(tooltipItem, data) {
-               var label = data.labels[tooltipItem.index];
-               return label + ': (' + tooltipItem.xLabel + ', ' + tooltipItem.yLabel + ')';
-            }
-         }
-      }
-   }
-});
+var imported_data = $.getJSON("../data/plotting_file.json", function(){
+	console.log("imported")
+})
+	.done(function( data ) {
+		console.log(data)
+		onJSONLoad(data)
+	});
+
+function onJSONLoad(){
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var scatterChart = new Chart(ctx, {
+       type: 'scatter',
+       data: {
+          labels: ["Label 1", "Label 2", "Label 3"],
+          datasets: [{
+             label: 'Legend',
+             data: [{
+                x: -10,
+                y: 0,
+             }, {
+                x: 0,
+                y: 10
+             }, {
+                x: 10,
+                y: 5
+             }]
+          }]
+       },
+       options: {
+          tooltips: {
+             callbacks: {
+                label: function(tooltipItem, data) {
+                   var label = data.labels[tooltipItem.index];
+                   return label + ': (' + tooltipItem.xLabel + ', ' + tooltipItem.yLabel + ')';
+                }
+             }
+          }
+       }
+    });
+
+}
