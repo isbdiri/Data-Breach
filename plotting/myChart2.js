c11993c85c1eeabb0ebcd8ef9bf430d0ab80bdaa
deutranium <29124655+deutranium@users.noreply.github.com>
2021-06-15 20:50:04 +0530

    Added jsn data

--- plotting/myChart2.js
+++ plotting/myChart2.js
@@ -13,25 +13,13 @@ var imported_data = $.getJSON("../data/plotting_file.json", function(){
 		onJSONLoad(data)
 	});
 
-function onJSONLoad(){
+function onJSONLoad(jsonData){
     var ctx = document.getElementById('myChart').getContext('2d');
     var scatterChart = new Chart(ctx, {
        type: 'scatter',
        data: {
-          labels: ["Label 1", "Label 2", "Label 3"],
-          datasets: [{
-             label: 'Legend',
-             data: [{
-                x: -10,
-                y: 0,
-             }, {
-                x: 0,
-                y: 10
-             }, {
-                x: 10,
-                y: 5
-             }]
-          }]
+          labels: jsonData.labels,
+          datasets: jsonData.datasets
        },
        options: {
           tooltips: {
