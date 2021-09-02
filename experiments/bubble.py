80e58026cc5f999f7f4133e151fd6e2485d5ca27
RishKhanna <55993921+RishKhanna@users.noreply.github.com>
2021-09-02 15:54:41 +0530

    updated req.txt

--- experiments/bubble.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv("../collect_data/final/Org_cats.csv")
-print(df)
-fig = px.scatter(df, x="category", y="Victim Count (Million)", color="category",
-                 size="Victim Count (Million)", text="Number of Breaches")
-fig.show()
--- experiments/pie.py
+++
@@ -1,9 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv("../collect_data/final/Org_cats.csv")
-
-fig = px.pie(df, values='Victim Count (Million)', names='category')
-fig.update_traces(textposition='inside')
-fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
-fig.show()
